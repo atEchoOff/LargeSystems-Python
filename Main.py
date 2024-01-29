e01e31770617aa50e6cc097a2da07e1a0e463857
Brian Christner <at.echo.off@outlook.com>
2024-01-29 13:56:32 -0600

    Using unicode to shorten function names

--- Main.py
+++ Main.py
@@ -3,21 +3,21 @@ from LargeSystems.linear import *
 import numpy as np
 from matplotlib import pyplot as plt
 
-def grad_lf(y):
+def Δlᶠ(y):
     return ZERO
 
-def grad_y_l(y, u, t):
+def Δᵧl(y, u, t):
     return 2 * y
 
-def grad_u_l(y, u, t):
+def Δᵤl(y, u, t):
     return 1 * u
 
 def f(y, u, t):
     return .5 * y + 1 * u
 
-jacobi_f_y = np.asmatrix(.5, dtype=np.float64)
+fᵧ = np.asmatrix(.5, dtype=np.float64)
 
-jacobi_f_u = np.asmatrix(1, dtype=np.float64)
+fᵤ = np.asmatrix(1, dtype=np.float64)
 
 t0 = 0
 y0 = 1
@@ -28,22 +28,22 @@ nu = 1
 
 
 solver = TMOCSolver(t0, y0, h, K, ny, nu)\
-         .with_grad_lf(grad_lf)\
-         .with_grad_y_l(grad_y_l)\
-         .with_grad_u_l(grad_u_l)\
+         .with_Δlᶠ(Δlᶠ)\
+         .with_Δᵧl(Δᵧl)\
+         .with_Δᵤl(Δᵤl)\
          .with_f(f)\
-         .with_jacobi_f_y(jacobi_f_y)\
-         .with_jacobi_f_u(jacobi_f_u)
+         .with_fᵧ(fᵧ)\
+         .with_fᵤ(fᵤ)
 
 solution = solver.solve(.5)
 
 y = [f"y{i}1" for i in range(0, K + 1)]
 u = [f"u{i}1" for i in range(0, K + 1)]
-_lambda = [f"lambda{i}1" for i in range(1, K + 1)]
+λ = [f"λ{i}1" for i in range(1, K + 1)]
 
 y = solution[y]
 u = solution[u]
-_lambda = solution[_lambda]
+λ = solution[λ]
 
 def true_y(t):
     return (2 * np.exp(3 * t) + np.exp(3)) / (np.exp(3 * t/2) * (2 + np.exp(3)))
@@ -51,25 +51,25 @@ def true_y(t):
 def true_u(t):
     return 2 * (np.exp(3 * t) - np.exp(3)) / (np.exp(3 * t / 2) * (2 + np.exp(3)))
 
-def true_lambda(t):
+def true_λ(t):
     return -2 * (np.exp(3 * t) - np.exp(3)) / (np.exp(3 * t / 2) * (2 + np.exp(3)))
 
 domain_y_u = np.linspace(0, 1, len(y))
-domain_lambda = np.linspace(0, 1, len(_lambda))
+domain_λ = np.linspace(0, 1, len(λ))
 
 plt.plot(domain_y_u, y, label="y")
-plt.plot(domain_y_u, true_y(domain_y_u), label="true_y")
+plt.plot(domain_y_u, true_y(domain_y_u), label="y*")
 plt.legend(loc="best")
 plt.show()
 
 
 plt.plot(domain_y_u, y, label="u")
-plt.plot(domain_y_u, true_u(domain_y_u), label="true_u")
+plt.plot(domain_y_u, true_u(domain_y_u), label="u*")
 plt.legend(loc="best")
 plt.show()
 
 
-plt.plot(domain_lambda, _lambda, label="lambda")
-plt.plot(domain_lambda, true_lambda(domain_lambda), label="true_lambda")
+plt.plot(domain_λ, λ, label="λ")
+plt.plot(domain_λ, true_λ(domain_λ), label="λ*")
 plt.legend(loc="best")
 plt.show()
--- TMOCSolver.py
+++ TMOCSolver.py
@@ -5,7 +5,7 @@ from ShiftedList import *
 
 class TMOCSolver:
     # A class designed for solving an optimal control problem of the form given in assignment 2
-    # Using the theta method, with equal stepsizes h
+    # Using the θ method, with equal stepsizes h
     # Due to the amount of parameters, this class will use the builder construct
 
     def __init__(self, t0, y0, h, K, ny, nu):
@@ -18,69 +18,69 @@ class TMOCSolver:
 
         # Initialize the variables
         self._y = ShiftedList(0, [ShiftedList(1, ["y" + str(i) + str(j) for j in range(1, ny + 1)]) for i in range(0, K + 1)])
-        self._lambda = ShiftedList(1, [ShiftedList(1, ["lambda" + str(i) + str(j) for j in range(1, ny + 1)]) for i in range(1, K + 1)])
+        self.λ = ShiftedList(1, [ShiftedList(1, ["λ" + str(i) + str(j) for j in range(1, ny + 1)]) for i in range(1, K + 1)])
         self._u = ShiftedList(0, [ShiftedList(1, ["u" + str(i) + str(j) for j in range(1, nu + 1)]) for i in range(0, K + 1)])
 
-        self.grad_lf = None
+        self.Δlᶠ = None
 
-        self.grad_y_l = None
-        self.grad_u_l = None
+        self.Δᵧl = None
+        self.Δᵤl = None
 
         self.f = None
-        self.jacobi_f_y = None
-        self.jacobi_f_u = None
+        self.fᵧ = None
+        self.fᵤ = None
     
-    def with_grad_lf(self, grad_lf):
-        self.grad_lf = grad_lf
+    def with_Δlᶠ(self, Δlᶠ):
+        self.Δlᶠ = Δlᶠ
         return self
     
-    def with_grad_y_l(self, grad_y_l):
-        self.grad_y_l = grad_y_l
+    def with_Δᵧl(self, Δᵧl):
+        self.Δᵧl = Δᵧl
         return self
     
-    def with_grad_u_l(self, grad_u_l):
-        self.grad_u_l = grad_u_l
+    def with_Δᵤl(self, Δᵤl):
+        self.Δᵤl = Δᵤl
         return self
     
     def with_f(self, f):
         self.f = f
         return self
     
-    def with_jacobi_f_y(self, jacobi_f_y):
-        self.jacobi_f_y = jacobi_f_y
+    def with_fᵧ(self, fᵧ):
+        self.fᵧ = fᵧ
         return self
     
-    def with_jacobi_f_u(self, jacobi_f_u):
-        self.jacobi_f_u = jacobi_f_u
+    def with_fᵤ(self, fᵤ):
+        self.fᵤ = fᵤ
         return self
     
-    def solve(self, theta):
-        # Solve the problem given theta
+    def solve(self, θ):
+        # Solve the problem given θ
         # A Solution object is returned
 
         # First, make sure we have everything from the builder pattern
-        if self.grad_lf == None:
-            print("grad_lf was not initialized")
+        if self.Δlᶠ == None:
+            print("Δlᶠ was not initialized")
             return
         
-        if self.grad_y_l == None:
-            print("grad_y_l was not initialized")
+        if self.Δᵧl == None:
+            print("Δᵧl was not initialized")
             return
         
-        if self.grad_u_l == None:
-            print("grad_u_l was not initialized")
+        if self.Δᵤl == None:
+            print("Δᵤl was not initialized")
             return
         
         if self.f == None:
             print("f was not initialized")
             return
         
-        if self.jacobi_f_y == None:
-            print("jacobi_f_y was not initialized")
+        if self.fᵧ == None:
+            print("fᵧ was not initialized")
             return
         
-        if self.jacobi_f_u == None:
-            print("jacobi_f_u was not initialized")
+        if self.fᵤ == None:
+            print("fᵤ was not initialized")
             return
         
         # Collect the variables into one long list
@@ -88,7 +88,7 @@ class TMOCSolver:
         for y in self._y.list:
             all_vars.extend(y.list)
         
-        for l in self._lambda.list:
+        for l in self.λ.list:
             all_vars.extend(l.list)
 
         for u in self._u.list:
@@ -97,7 +97,7 @@ class TMOCSolver:
         # Shorthand
         K = self.K
         y = ShiftedList(0, [V(*self._y[i].list) for i in range(0, K + 1)])
-        _lambda = ShiftedList(1, [V(*self._lambda[i].list) for i in range(1, K + 1)])
+        λ = ShiftedList(1, [V(*self.λ[i].list) for i in range(1, K + 1)])
         u = ShiftedList(0, [V(*self._u[i].list) for i in range(0, K + 1)])
         h = self.h
 
@@ -106,40 +106,40 @@ class TMOCSolver:
         builder = SystemBuilder(all_vars)
 
         # First, add the lambda_k constraint
-        builder.add_constraint(0 == 1 * _lambda[K] + self.grad_lf(y[K])
-                                    + h * (1 - theta) * self.grad_y_l(y[K], u[K], t[K])\
-                                    - h * (1 - theta) * self.jacobi_f_y.T * _lambda[K])
+        builder.add_constraint(0 == 1 * λ[K] + self.Δlᶠ(y[K])
+                                    + h * (1 - θ) * self.Δᵧl(y[K], u[K], t[K])\
+                                    - h * (1 - θ) * self.fᵧ.T * λ[K])
 
         for k in range(0, K - 1):
             # Add constraint from (9)
-            builder.add_constraint(0 == 1 * _lambda[k + 1]
-                                        - h * (1 - theta) * self.jacobi_f_y.T * _lambda[k + 1]
-                                        + h * self.grad_y_l(y[k + 1], u[k + 1], t[k + 1])
-                                        - 1 * _lambda[k + 2]
-                                        - h * theta * self.jacobi_f_y.T * _lambda[k + 2])
+            builder.add_constraint(0 == 1 * λ[k + 1]
+                                        - h * (1 - θ) * self.fᵧ.T * λ[k + 1]
+                                        + h * self.Δᵧl(y[k + 1], u[k + 1], t[k + 1])
+                                        - 1 * λ[k + 2]
+                                        - h * θ * self.fᵧ.T * λ[k + 2])
             
         
         # Add the constraint from (10)
-        builder.add_constraint(0 == h * theta * self.grad_u_l(y[0], u[0], t[0])
-                                    - h * theta * self.jacobi_f_u.T * _lambda[1])
+        builder.add_constraint(0 == h * θ * self.Δᵤl(y[0], u[0], t[0])
+                                    - h * θ * self.fᵤ.T * λ[1])
         
         # Add the constraint from (11)
-        builder.add_constraint(0 == h * (1 - theta) * self.grad_u_l(y[K], u[K], t[K])
-                                    - h * (1 - theta) * self.jacobi_f_u.T * _lambda[K])
+        builder.add_constraint(0 == h * (1 - θ) * self.Δᵤl(y[K], u[K], t[K])
+                                    - h * (1 - θ) * self.fᵤ.T * λ[K])
         
         for k in range(1, K):
             # Add the constraint from (12)
-            builder.add_constraint(0 == h * self.grad_u_l(y[k], u[k], t[k])
-                                        - h * theta * self.jacobi_f_u.T * _lambda[k + 1]
-                                        - h * (1 - theta) * self.jacobi_f_u.T * _lambda[k])
+            builder.add_constraint(0 == h * self.Δᵤl(y[k], u[k], t[k])
+                                        - h * θ * self.fᵤ.T * λ[k + 1]
+                                        - h * (1 - θ) * self.fᵤ.T * λ[k])
             
-        # Add theta method base case
+        # Add θ method base case
         builder.add_constraint(self.y0 == 1 * y[0])
 
-        # Add all theta method steps
+        # Add all θ method steps
         for k in range(0, K):
             builder.add_constraint(0 == 1 * y[k] - 1 * y[k + 1]
-                                        + h * theta * self.f(y[k], u[k], t[k]) 
-                                        + h * (1 - theta) * self.f(y[k + 1], u[k + 1], t[k + 1]))
+                                        + h * θ * self.f(y[k], u[k], t[k]) 
+                                        + h * (1 - θ) * self.f(y[k + 1], u[k + 1], t[k + 1]))
         
         return builder.solve()
